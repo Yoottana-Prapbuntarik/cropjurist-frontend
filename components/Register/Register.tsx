31aad5300ecede24d0cddd916c6ce3058da98e31
nap3831 <nap3831@gmail.com>
2020-03-16 22:08:08 +0700

    Create page Register

---
+++ components/Register/Register.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+const Register = () =>{
+    return(
+        <form className="text-center">
+            <h1>Sign up</h1>
+        </form>
+    )
+};
+export default Register;
