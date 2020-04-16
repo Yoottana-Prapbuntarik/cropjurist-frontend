83e32759b961111d567068e9778bed5885aa5f13
nap3831 <nap3831@gmail.com>
2020-04-16 20:51:16 +0700

    Updated regex and move regex without this

--- Validated/SigninValidator/SigninValidator.tsx
+++ Validated/SigninValidator/SigninValidator.tsx
@@ -1,24 +1,16 @@
 import { FormErrors } from "redux-form";
-import { ErrorField } from "../InterfaceValidator";
+import { ErrorField, regexExpression } from "../InterfaceValidator";
 
-const validate = (data, { signinPresenter, t }: any): FormErrors => {
-  let regexEmail: RegExp = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i;
-  let WhiteSpace = new RegExp(/^\s+$/);
+const validate = (dataSignin, { signinPresenter, t }: any): FormErrors => {
   let errors: FormErrors<ErrorField> = {};
-  if (!data.email) {
-    errors.email = t(signinPresenter.errorMessageForm.keyEmailErrorMessage);
-  } else if (!regexEmail.test(data.email)) {
-    errors.email = t(signinPresenter.errorMessageForm.keyEmailErrorMessage);
+  if (!regexExpression.regexEmail.test(dataSignin.email) || 
+      !dataSignin.email) {
+      errors.email = t(signinPresenter.messageForm.keyEmailErrorMessage);
   }
-  if (data.password === undefined || data.password.length < 5) {
-    errors.password = t(
-      signinPresenter.errorMessageForm.keyPasswordErrorMessage
-    );
-  } else if (WhiteSpace.test(data.password)) {
-    errors.password = t(
-      signinPresenter.errorMessageForm.keyPasswordErrorMessage
-    );
-  }
+  if (!regexExpression.regexPassword.test(dataSignin.password) || 
+      !dataSignin.password) {
+      errors.password = t(signinPresenter.messageForm.keyPasswordErrorMessage)
+      }
   return errors;
 };
 
