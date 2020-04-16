3a3b1fe9e8c70bf6dedd778c54f570568f7f082a
nap3831 <nap3831@gmail.com>
2020-04-16 19:33:54 +0700

    Created object regexExpression

--- Validated/InterfaceValidator.tsx
+++ Validated/InterfaceValidator.tsx
@@ -1,8 +1,14 @@
 export interface ErrorField {
   email: string;
-  password:string;
-  name:string;
-  lastName:string;
-  confirmEmail:string;
-  confirmPassword:string;
+  password: string;
+  name: string;
+  lastName: string;
+  confirmEmail: string;
+  confirmPassword: string;
 }
+
+export const regexExpression = {
+  regexEmail: new RegExp(/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i),
+  regexText: new RegExp(/^[a-zA-Zก-๙]*$/),
+  regexPassword: new RegExp(/^[a-zA-Z0-9]*$/),
+};
