1f86fce55f7eba126d6434476dfa5e9e46fc0dc0
nap3831 <nap3831@gmail.com>
2020-04-21 16:53:38 +0700

    Refatered Code Member page

--- pages/member.jsx
+++ pages/member.jsx
@@ -4,11 +4,9 @@ import LandingLayout from "../components/LandingLayout/LandingLayout";
 const Member = () => {
   return (
     <LandingLayout>
-     
           <h1 className="text-success justify-content-center d-flex  align-content-center align-item-center ">
               Hello Welcome back !
           </h1>
-
     </LandingLayout>
   );
 };
