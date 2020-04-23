f05a7832109fde7839ba0b43b4e16e0736f7a171
kittinun chobthum <chobtham.kittinun@gmail.com>
2020-04-23 16:16:27 +0700

    Refactored Landing layout.

--- components/LandingLayout/LandingLayout.tsx
+++ components/LandingLayout/LandingLayout.tsx
@@ -1,21 +1,21 @@
-import Head from "next/head";
-import Navigation from "../Navigation/NavigationContainer";
-import Footer from "../Footer/FooterViewContainer";
-import { withTranslation } from "../../i18n";
+import Head from 'next/head';
+import Navigation from '../Navigation/NavigationContainer';
+import Footer from '../Footer/FooterViewContainer';
+import { withTranslation } from '../../i18n';
 
 const LandingLayout = (props: any) => (
-  <div className="page-wrapper">
-    <Head>
-      <title>{props.t("landingTitle")}</title>
+	<div className="page-wrapper">
+		<Head>
+			<title>{props.t('landingTitle')}</title>
 
-      <link href="styles/plugins/theme-plugin.css" rel="stylesheet" />
-      <script src="assets/js/theme-plugin.js"></script>
-      <script src="assets/js/theme-script.js"></script>
-    </Head>
-    <Navigation />
-    {props.children}
-    <Footer />
-  </div>
+			<link href="styles/plugins/theme-plugin.css" rel="stylesheet" />
+			<script src="assets/js/theme-plugin.js" />
+			<script src="assets/js/theme-script.js" />
+		</Head>
+		<Navigation />
+		{props.children}
+		<Footer />
+	</div>
 );
 
-export default withTranslation("common")(LandingLayout);
+export default withTranslation('common')(LandingLayout);
