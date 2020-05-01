08748af2d653550960fbf3e2aef023c6e2806ea2
nap3831 <nap3831@gmail.com>
2020-05-01 13:09:26 +0700

    Refactor code manage space and change name forgot reducer to camel case

--- components/ForgotPassword/ForgotPasswordContainer.tsx
+++ components/ForgotPassword/ForgotPasswordContainer.tsx
@@ -1,29 +1,29 @@
 import { connect } from 'react-redux';
 import { reduxForm, reset } from 'redux-form';
-import { ForgotAction, fotgotPassword } from '../../apis/forgotPasswordAPIClient';
+import { ForgotAction, fotgotPasswordForm } from '../../apis/forgotPasswordAPIClient';
 import { withTranslation, i18n } from '../../i18n';
 import { ForgotPassworPresenter } from './ForgotPasswordInterface';
 import { Dispatch } from 'redux';
 import { FormManager } from '../../manager/formManager';
 import ForgotPassword from './ForgotPassword';
-import validate from '../../validator/ForgotPasswordValidator/ForgotPassword';
+import validate from '../../validator/forgotPasswordValidator/forgotPasswordValidator';
 
 const forgotPassworPresenter: ForgotPassworPresenter = {
     keyEmail: "email",
     keyHeader: "forgotHeader",
     keySubtitle: "forgotySubtitle",
-    keyButtonName: "btnForgot",
+    keyButtonName: "buttonForgotPassword",
 	keyBackToLogin: "backToLogin",
-	keyimagePath: '/assets/images/forgotpassword/forgotpassword.jpg'
+	keyImagePath: "/assets/images/forgotpassword/forgotpassword.jpg"
 }
 
-export const ForgotPasswordReducer = (state: ForgotPassworPresenter = forgotPassworPresenter, action: any) => {
+export const forgotPasswordReducer = (state: ForgotPassworPresenter = forgotPassworPresenter, action: any) => {
 	switch (action.type) {
 		case ForgotAction.ResetPassword_Success:
 			alert(i18n.t(action.keyMessage));
 			return state;
-			case ForgotAction.ResetPassword_Failed:
-				alert(i18n.t(action.keyMessage));
+		case ForgotAction.ResetPassword_Failed:
+			alert(i18n.t(action.keyMessage));
 			return state;
 		default:
 			return state;
@@ -35,11 +35,11 @@ const mapStateToProps = (state: any) => ({
 
 const mapDispatchToProps = (dispatch: Dispatch) => ({
 	submitResetPassword: (event: any) => {
-		dispatch(fotgotPassword(event.email));
-		dispatch(reset(FormManager.ForgotForm));
+		dispatch(fotgotPasswordForm(event.email));
+		dispatch(reset(FormManager.ForgotPasswordForm));
 	}
 });
 
-const form = reduxForm({ form: FormManager.ForgotForm, validate })(ForgotPassword);
+const form = reduxForm({ form: FormManager.ForgotPasswordForm, validate })(ForgotPassword);
 
 export default withTranslation('common')(connect(mapStateToProps, mapDispatchToProps)(form));
