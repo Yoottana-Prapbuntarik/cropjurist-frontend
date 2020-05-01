e7a319d95c9d7606c1c30f4f64461690bdf5e158
nap3831 <nap3831@gmail.com>
2020-05-01 13:00:04 +0700

    Edited name parameter signin to forgotPassword

---
+++ Validator/ForgotPasswordValidator/forgotPasswordValidator.tsx
@@ -0,0 +1,14 @@
+import { FormErrors } from 'redux-form';
+import { ErrorField } from '../InterfaceValidator';
+import { emailValidator } from '../emailValidator/emailValidator';
+
+const validate = (forgotPassword: any, { t }: any): FormErrors => {
+	let errors: FormErrors<ErrorField> = {};
+	let emailValidatorResult = emailValidator(forgotPassword.email);
+	if (!emailValidatorResult.status) {
+		errors.email = t(emailValidatorResult.keyMessage);
+	}
+	return errors;
+};
+
+export default validate;
