cef4efa339f05c9953387f60a182762da4d1ed4b
nap3831 <nap3831@gmail.com>
2020-05-10 23:51:39 +0700

    Created IncorporationManager control field

---
+++ components/IncorporationManager/IncorporationManagerContainer.tsx
@@ -0,0 +1,39 @@
+import { connect } from 'react-redux';
+import { withTranslation } from '../../i18n';
+import {
+    MemberPresenter,
+    MemberItems,
+    LinkItems,
+    MemberDialog
+} from './IncorporationManagerInterface';
+import IncorporationManager from './IncorporationManager';
+
+const linkItems: LinkItems[] = [
+    { keyTitlePath: "companyInformation", routePath: "/companyInformation" },
+]
+
+const memberDialog: MemberDialog = {
+    keyDialogTitle: "dialogTitle",
+    keyDialogSubTitle: "dialogSubTitle",
+    keyDialogDetail: "dialogDetai",
+    keyCloseTab: "close"
+}
+
+const memberItems: MemberItems = {
+    linkItems: linkItems
+};
+
+const memberPresenter: MemberPresenter = {
+    memberItems: memberItems,
+    memberDialog: memberDialog
+};
+
+export const inCorporationManagerReducer = (state: MemberPresenter = memberPresenter) => {
+    return state;
+};
+
+const mapStateToProps = (state: any) => ({
+    memberPresenter: state.inCorporationManagerReducer
+});
+
+export default withTranslation('common')(connect(mapStateToProps)(IncorporationManager));
