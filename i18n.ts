3586b9ce722f4090382a1507289907f6fe1a3363
kittinun chobthum <kittinun@corpjurist.com>
2020-05-14 23:50:34 +0700

    Chaged local sub paths.

--- i18n.ts
+++ i18n.ts
@@ -1,21 +1,12 @@
-const NextI18Next = require("next-i18next/dist/commonjs").default;
+const NextI18Next = require('next-i18next/dist/commonjs').default;
 
 const NextI18NextInstance = new NextI18Next({
-  defaultLanguage: "en",
-  otherLanguages: ["th"],
-  localeSubpaths: {
-    th: "thai",
-    en: "eng"
-  }
+	defaultLanguage: 'en',
+	otherLanguages: [ 'th' ]
 });
 
 module.exports = NextI18NextInstance;
 
 export default NextI18NextInstance;
 
-export const {
-  appWithTranslation,
-  withTranslation,
-  i18n,
-  Link
-} = NextI18NextInstance;
+export const { appWithTranslation, withTranslation, i18n, Link } = NextI18NextInstance;
