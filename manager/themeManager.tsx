a2c4771ae7cf61ab572b8e54e7ef4a9994ad18bf
nap3831 <nap3831@gmail.com>
2020-05-18 02:08:31 +0700

    Created themeManager for control styles and javascript theme

---
+++ manager/themeManager.tsx
@@ -0,0 +1,8 @@
+export enum themeManager {
+    StylesMainFolder = "styles/plugins/theme-plugin.css",
+    ThemePluginMainFolder = "assets/js/theme-plugin.js",
+    ThemeScriptMainFolder = "assets/js/theme-script.js",
+    StylesMainSubFolder = "../../styles/plugins/theme-plugin.css",
+    ThemePluginMainSubFolder = "../../assets/js/theme-plugin.js",
+    ThemeScriptMainSubFolder = "../../assets/js/theme-script.js",
+}
