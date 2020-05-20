093bd0ec40b7c0974b6e5243049a4e6086a531b9
nap3831 <nap3831@gmail.com>
2020-05-20 21:46:56 +0700

    Created route path after change password

--- components/ChangePassword/ChangePasswordContainer.tsx
+++ components/ChangePassword/ChangePasswordContainer.tsx
@@ -7,22 +7,26 @@ import { FormManager } from '../../manager/formManager';
 import { changePassword, changePasswordAction } from '../../apis/changePasswordAPIClient';
 import { withTranslation, i18n } from '../../i18n';
 import { Dispatch } from 'redux';
+import Router from 'next/router';
+import { routeToSignin } from '../../manager/routerManager';
 
 const changePasswordPresenter: ChangePasswordPresenter = {
     keyTitleChangePassword: "titleChangePassword",
     keyNewPassword: "newPassword",
     keyConfirmNewPassword: "placeholderConfirmPasswordSignup",
     keySubmitResetPassword: "changePassword",
-    keyImagePath: '../../assets/images/changepassword/changepassword.jpg'
+    keyImagePath: '../../../assets/images/changepassword/changepassword.jpg'
 }
 
 export const changePasswordReducer = (state: ChangePasswordPresenter = changePasswordPresenter, action: any) => {
     switch (action.type) {
         case changePasswordAction.changePassword_Success:
             alert(i18n.t(action.keyMessage));
+            Router.push(routeToSignin);
             return state;
         case changePasswordAction.changePassword_Failed:
             alert(i18n.t(action.keyMessage));
+            Router.push(routeToSignin);
             return state;
         default:
             return state;
@@ -33,9 +37,9 @@ const mapStateToProps = (state: any) => ({
     changePasswordPresenter: state.changePasswordReducer
 })
 
-const mapDispatchToProps = (dispatch: Dispatch) => ({
+const mapDispatchToProps = (dispatch: Dispatch, ownProps: any) => ({
     changePassword: (event: any) => {
-        dispatch(changePassword(event.password));
+        dispatch(changePassword(event.password, ownProps.tokenID, ownProps.uuId));
         dispatch(reset(FormManager.ChangePasswordForm));
     }
 });
