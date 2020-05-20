798bc37e34bc2055d553572169c7def5fc49f6db
nap3831 <nap3831@gmail.com>
2020-05-20 21:50:23 +0700

    Created manage css and js in project

--- manager/themeManager.tsx
+++ manager/themeManager.tsx
@@ -2,7 +2,7 @@ export enum themeManager {
     StylesMainFolder = "styles/plugins/theme-plugin.css",
     ThemePluginMainFolder = "assets/js/theme-plugin.js",
     ThemeScriptMainFolder = "assets/js/theme-script.js",
-    StylesMainSubFolder = "../../styles/plugins/theme-plugin.css",
-    ThemePluginMainSubFolder = "../../assets/js/theme-plugin.js",
-    ThemeScriptMainSubFolder = "../../assets/js/theme-script.js",
+    StylesMainSubFolder = "../../../styles/plugins/theme-plugin.css",
+    ThemePluginMainSubFolder = "../../../assets/js/theme-plugin.js",
+    ThemeScriptMainSubFolder = "../../../assets/js/theme-script.js",
 }
