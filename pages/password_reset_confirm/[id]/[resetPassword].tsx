1fcf4b86e47e9f345aad8d46e5e582536bd64237
nap3831 <nap3831@gmail.com>
2020-05-20 21:51:53 +0700

    Parse props to reducer for post api

--- pages/password_reset_confirm/[id]/[resetPassword].tsx
+++ pages/password_reset_confirm/[id]/[resetPassword].tsx
@@ -2,8 +2,11 @@ import React from 'react';
 import LandingLayout from '../../../components/LandingLayout/LandingLayout';
 import ChangePassword from '../../../components/ChangePassword/ChangePasswordContainer';
 import { themeManager } from '../../../manager/themeManager';
+import { useRouter } from 'next/router';
 
 const resetPassword = () => {
+	const router = useRouter();
+
 	return (
 		<LandingLayout
 			styleTheme={themeManager.StylesMainSubFolder}
@@ -11,7 +14,7 @@ const resetPassword = () => {
 			themeScript={themeManager.ThemeScriptMainSubFolder}
 		>
 			<div className="min-vh-100">
-				<ChangePassword />
+				<ChangePassword tokenID={router.query.id} uuId={router.query.resetPassword}/>
 			</div>
 		</LandingLayout>
 	);
