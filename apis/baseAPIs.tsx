e0f07ac845f312febca3a9981c71bb839c3d2372
nap3831 <nap3831@gmail.com>
2020-05-26 02:24:08 +0700

    Added Interceptors

--- apis/baseAPIs.tsx
+++ apis/baseAPIs.tsx
@@ -1,9 +1,9 @@
 import axios from 'axios';
+import join from 'url-join'
 
 // const productionURL = 'https://corpjurist.com/api/v1/';
 const devURL = 'https://dev.corpjurist.com/api/v1/';
 const devURLResetPassword = 'https://dev.corpjurist.com/api/';
-
 const service = axios.create({
 	baseURL: devURL,
 	headers: { 'content-type': 'application/json' }
@@ -15,3 +15,17 @@ export const serviceResetPassword = axios.create({
 	baseURL: devURLResetPassword,
 	headers: { 'content-type': 'application/json' }
 });
+
+axios.interceptors.request.use(async (config) => {
+	const token = localStorage.getItem("access-token");
+	if (token != null) {
+		config.headers = { 
+			'Content-Type': 'application/json',
+			'Authorization': 'Token '+token,
+		 }
+	}
+	config.url = join(devURL, config.url)
+	return config;
+})
+
+export const serviceSetToken = axios;
