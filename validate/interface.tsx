d6df4154f29d65bef6c6decaf010d96d34c28303
nap3831 <nap3831@gmail.com>
2020-05-26 03:11:07 +0700

    Updated regex

--- validate/interface.tsx
+++ validate/interface.tsx
@@ -22,7 +22,7 @@ export interface ErrorField {
 
 export const regexExpression = {
 	regexEmail: new RegExp(/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i),
-	regexText: new RegExp(/^[a-zA-Z0-9ก-๙]*$/),
+	regexText: new RegExp(/^[a-zA-Z0-9ก-๙// ]*$/),
 	regexTextOrWhiteSpace: new RegExp(/^[a-zA-Z0-9ก-๙]*$/),
 	regexPassword: new RegExp(/^[a-zA-Z0-9]{5,}$/)
 };
