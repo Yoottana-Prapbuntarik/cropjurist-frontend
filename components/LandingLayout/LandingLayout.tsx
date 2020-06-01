686932efc5bf418e1b1cee5aab1a9eb424186252
nap3831 <nap3831@gmail.com>
2020-06-01 23:20:05 +0700

    Checked for user token

--- components/LandingLayout/LandingLayout.tsx
+++ components/LandingLayout/LandingLayout.tsx
@@ -2,20 +2,25 @@ import Head from 'next/head';
 import Navigation from '../Navigation/NavigationContainer';
 import Footer from '../Footer/FooterViewContainer';
 import { withTranslation } from '../../i18n';
+import checkToken from '../../user/checkToken';
 
-const LandingLayout = (props: any) => (
-	<div className="page-wrapper">
-		<Head>
-			<title>{props.t('landingTitle')}</title>
+const LandingLayout = (props: any) => {
+	checkToken();
+	
+	return (
+		<div className="page-wrapper">
+			<Head>
+				<title>{props.t('landingTitle')}</title>
 
-			<link href={props.styleTheme} rel="stylesheet" />
-			<script src={props.themePlugin} />
-			<script src={props.themeScript} />
-		</Head>
-		<Navigation />
-		{props.children}
-		<Footer/>
-	</div>
-);
+				<link href={props.styleTheme} rel="stylesheet" />
+				<script src={props.themePlugin} />
+				<script src={props.themeScript} />
+			</Head>
+			<Navigation />
+			{props.children}
+			<Footer />
+		</div>
+	)
+};
 
 export default withTranslation('common')(LandingLayout);
