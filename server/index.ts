15437e83d38a32939d818bca0b10fc324a2efd1a
KittinunCho <kittinun_cho@truecorp.co.th>
2020-06-20 13:23:29 +0700

    refactor index.ts from server.

--- server/index.ts
+++ server/index.ts
@@ -1,28 +1,34 @@
-const express = require("express");
-const next = require("next");
-const nextI18NextMiddleware = require("next-i18next/middleware").default;
+import express from 'express'
+import next from 'next'
 
-const nextI18next = require("../i18n");
+const nextI18NextMiddleware = require('next-i18next/middleware').default
 
-const port = process.env.PORT || 3000;
-const app = next({ dev: process.env.NODE_ENV !== "production" });
-const handle = app.getRequestHandler();
+const nextI18next = require('../i18n')
+
+const port = process.env.PORT || 3000
+const app = next({ dev: process.env.NODE_ENV !== 'production' })
+const handle = app.getRequestHandler()
+
+async function printError (error: any) {
+  console.log(error)
+}
 
 (async () => {
-  await app.prepare();
-  const server = express();
+  await app.prepare()
+  const server = express()
 
   try {
-    server.use(nextI18NextMiddleware(nextI18next));
+    server.use(nextI18NextMiddleware(nextI18next))
   } catch (e) {
-    throw e;
+    printError(e)
+    throw e
   }
 
   // handle nextjs routing
-  server.get("*", (req: any, res: any) => handle(req, res));
+  server.get('*', (req: any, res: any) => handle(req, res))
 
-  server.listen(port);
-  console.log(`🚀 Ready on http://localhost:${port}`); // eslint-disable-line no-console
-})();
+  server.listen(port)
+  console.log(`🚀 Ready on http://localhost:${port}`) // eslint-disable-line no-console
+})()
 
-export default undefined;
+export default undefined
