cf1edc6e829f16b45cb14fb542446cb804927e22
nap3831 <nap3831@gmail.com>
2020-06-23 02:05:09 +0700

    refactor code

--- apis/companyInformationAPIClient.tsx
+++ apis/companyInformationAPIClient.tsx
@@ -1,6 +1,6 @@
-import { Dispatch } from "redux";
-import { serviceSetToken } from "../apis/baseAPIs";
-import { KeyManager } from '../manager/keyManager';
+import { Dispatch } from 'redux'
+import { serviceSetToken } from '../apis/baseAPIs'
+import { KeyManager } from '../manager/keyManager'
 
 export enum SendInformationCompany {
     SendInformationCompany_Success = 'SendInformationCompany_Success',
@@ -8,90 +8,79 @@ export enum SendInformationCompany {
 }
 
 export const companyInformationForm: any = (company_name_1: string, company_name_2: string, company_name_3: string,
-    registration_no: string, address_no: string, village: string, road: string,
-    province: string, district: string, sub_district: string, zipcode: string,
-    license_number: string, auditor_name: string) => async (dispatch: Dispatch
-    ) => {
+  registration_no: string, address_no: string, village: string, road: string,
+  province: string, district: string, sub_district: string, zipcode: string,
+  license_number: string, auditor_name: string) => async (dispatch: Dispatch
+) => {
+  serviceSetToken({
+    method: 'post',
+    url: 'company_information/',
+    data: params(company_name_1, company_name_2, company_name_3,
+      registration_no, address_no, village, road,
+      province, district, sub_district, zipcode,
+      license_number, auditor_name)
+  })
 
-        serviceSetToken({
-            method: 'post',
-            url: 'company_information/',
-            data: params(company_name_1, company_name_2, company_name_3,
-                registration_no, address_no, village, road,
-                province, district, sub_district, zipcode,
-                license_number, auditor_name)
+    .then((response) => {
+      if (response) {
+        dispatch({
+          type: SendInformationCompany.SendInformationCompany_Success,
+          keyMessage: KeyManager.SendInformationCompanySuccess
         })
+      }
+    })
+}
 
-            .then((response) => {
-                if (response) {
-                    dispatch({
-                        type: SendInformationCompany.SendInformationCompany_Success,
-                        keyMessage: KeyManager.SendInformationCompanySuccess
-                    });
-                }
-            })
+export const updateCompanyInformationForm: any = (currentIdInformation: number, company_name_1: string, company_name_2: string, company_name_3: string,
+  registration_no: string, address_no: string, village: string, road: string,
+  province: string, district: string, sub_district: string, zipcode: string,
+  license_number: string, auditor_name: string) => async (dispatch: Dispatch
+) => {
+  serviceSetToken({
+    method: 'put',
+    url: `company_information/${currentIdInformation}/edit/`,
+    data: params(company_name_1, company_name_2, company_name_3,
+      registration_no, address_no, village, road,
+      province, district, sub_district, zipcode,
+      license_number, auditor_name)
+  })
 
-            .catch((error) => {
-                if (error) {
-                    dispatch({
-                        type: SendInformationCompany.SendInformationCompany_Failed,
-                        keyMessage: error.response.data[0]
-                    });
-                }
-            });
-    };
-
-export const updateCompanyInformationForm: any = (currentIdInformation: number,company_name_1: string, company_name_2: string, company_name_3: string,
-    registration_no: string, address_no: string, village: string, road: string,
-    province: string, district: string, sub_district: string, zipcode: string,
-    license_number: string, auditor_name: string) => async (dispatch: Dispatch
-    ) => {
-
-        serviceSetToken({
-            method: 'put',
-            url: `company_information/${currentIdInformation}/edit/`,
-            data: params(company_name_1, company_name_2, company_name_3,
-                registration_no, address_no, village, road,
-                province, district, sub_district, zipcode,
-                license_number, auditor_name)
+    .then((response) => {
+      if (response) {
+        dispatch({
+          type: SendInformationCompany.SendInformationCompany_Success,
+          keyMessage: KeyManager.SendInformationCompanySuccess
         })
+      }
+    })
 
-            .then((response) => {
-                if (response) {
-                    dispatch({
-                        type: SendInformationCompany.SendInformationCompany_Success,
-                        keyMessage: KeyManager.SendInformationCompanySuccess
-                    });
-                }
-            })
+    .catch((error) => {
+      if (error) {
+        dispatch({
+          type: SendInformationCompany.SendInformationCompany_Failed,
+          keyMessage: error.response.data[0]
+        })
+      }
+    })
+}
 
-            .catch((error) => {
-                if (error) {
-                    dispatch({
-                        type: SendInformationCompany.SendInformationCompany_Failed,
-                        keyMessage: error.response.data[0]
-                    });
-                }
-            });
-    };
-    
 const params = (company_name_1: string, company_name_2: string, company_name_3: string,
-    registration_no: string, address_no: string, village: string, road: string,
-    province: string, district: string, sub_district: string, zipcode: string,
-    license_number: string, auditor_name: string) => {
-    return {
-        company_name_1: company_name_1,
-        company_name_2: company_name_2,
-        company_name_3: company_name_3,
-        registration_no: registration_no,
-        address_no: address_no,
-        village: village,
-        road: road,
-        province: province,
-        district: district,
-        sub_district: sub_district,
-        zipcode: zipcode,
-        license_number: license_number,
-        auditor_name: auditor_name
-    };
+  registration_no: string, address_no: string, village: string, road: string,
+  province: string, district: string, sub_district: string, zipcode: string,
+  license_number: string, auditor_name: string) => {
+  return {
+    company_name_1: company_name_1,
+    company_name_2: company_name_2,
+    company_name_3: company_name_3,
+    registration_no: registration_no,
+    address_no: address_no,
+    village: village,
+    road: road,
+    province: province,
+    district: district,
+    sub_district: sub_district,
+    zipcode: zipcode,
+    license_number: license_number,
+    auditor_name: auditor_name
+  }
 }
--- apis/signinAPIClient.tsx
+++ apis/signinAPIClient.tsx
@@ -1,6 +1,6 @@
-import service from './baseAPIs';
-import { Dispatch } from 'redux';
-import { KeyManager } from '../manager/keyManager';
+import service from './baseAPIs'
+import { Dispatch } from 'redux'
+import { KeyManager } from '../manager/keyManager'
 
 export enum SigninAction {
 	Signin_Success = 'Signin_Success',
@@ -8,41 +8,41 @@ export enum SigninAction {
 }
 
 export const signin: any = (email: string, password: string, isRemember?: boolean) => async (dispatch: Dispatch) => {
-	service({
-		method: 'post',
-		url: 'user/login/',
-		data: params(email, password, isRemember)
-	})
-		.then((response) => {
-			if (response) {
-				dispatch({
-					type: SigninAction.Signin_Success
-				});
-			}
+  service({
+    method: 'post',
+    url: 'user/login/',
+    data: params(email, password, isRemember)
+  })
+    .then((response) => {
+      if (response) {
+        dispatch({
+          type: SigninAction.Signin_Success
+        })
+      }
 
-			localStorage.setItem('access-token', response.data.token);
-		})
-		.catch((error) => {
-			if (error) {
-				dispatch({
-					type: SigninAction.Signin_Failed,
-					keyMessage: error.response.data[KeyManager.Message]
-				});
-			}
-		});
-};
+      localStorage.setItem('access-token', response.data.token)
+    })
+    .catch((error) => {
+      if (error) {
+        dispatch({
+          type: SigninAction.Signin_Failed,
+          keyMessage: error.response.data[KeyManager.Message]
+        })
+      }
+    })
+}
 
 const params = (email: string, password: string, isRemember?: boolean) => {
-	if (isRemember == undefined) {
-		return {
-			email: email,
-			password: password
-		};
-	}
+  if (isRemember == undefined) {
+    return {
+      email: email,
+      password: password
+    }
+  }
 
-	return {
-		email: email,
-		password: password,
-		is_remember: isRemember
-	};
-};
+  return {
+    email: email,
+    password: password,
+    is_remember: isRemember
+  }
+}
