fcee448a5b03572f07bb7de9c39a4ff326af564e
nap3831 <nap3831@gmail.com>
2020-06-29 03:38:10 +0700

    Created form Add shareholder  in modal

---
+++ components/AddShareHolderComponent/ShareHolderManagerPresenter.tsx
@@ -0,0 +1,121 @@
+import {
+  ShareHolderManagerPresenter,
+  LabelFirstName,
+  LabelMiddleName,
+  LabelFamilyName,
+  LabelPassportId,
+  LabelEmail,
+  LabelTelephone,
+  LabelAddressCheckbox,
+  LabelAddressNumber,
+  LabelVillage,
+  LabelRoad,
+  LabelProvicne,
+  LabelDistrict,
+  LabelSubDistrict,
+  LabelZipCode,
+  ButtonSubmitProperty
+} from './ShareHolderManagerInterface'
+
+import { KeyManager } from '../../manager/keyManager'
+
+const labelFirstName: LabelFirstName = {
+  name: KeyManager.FirstName,
+  valueName: ''
+}
+
+const labelMiddleName: LabelMiddleName = {
+  name: KeyManager.MiddleName,
+  valueMiddleName: ''
+}
+
+const labelFamilyName: LabelFamilyName = {
+  name: KeyManager.MiddleName,
+  valueFamilyName: ''
+}
+
+const labelPassportId: LabelPassportId = {
+  name: KeyManager.Passport,
+  valuePassportId: ''
+}
+
+const labelEmail: LabelEmail = {
+  name: KeyManager.Email,
+  valueEmail: ''
+}
+
+const labelTelephone: LabelTelephone = {
+  name: KeyManager.Telephone,
+  valueTelephone: ''
+}
+
+const labelAddressCheckbox: LabelAddressCheckbox = {
+  name: KeyManager.Address
+}
+
+const labelAddressNumber: LabelAddressNumber = {
+  name: KeyManager.AddressNumber,
+  valueAddressNumber: ''
+}
+
+const labelVillage: LabelVillage = {
+  name: KeyManager.Village,
+  valueVillage: ''
+}
+
+const labelRoad: LabelRoad = {
+  name: KeyManager.Road,
+  valueRoad: ''
+}
+
+const labelProvicne: LabelProvicne = {
+  name: KeyManager.Province,
+  valueProvince: ''
+}
+
+const labelDistrict: LabelDistrict = {
+  name: KeyManager.District,
+  valueDistrict: ''
+
+}
+
+const labelSubDistrict: LabelSubDistrict = {
+  name: KeyManager.SubDistrict,
+  valueSubDistrict: ''
+}
+
+const labelZipCode: LabelZipCode = {
+  name: KeyManager.ZipCode,
+  valueZipCode: ''
+}
+
+const buttonOtherShareHolder: ButtonSubmitProperty = {
+  name: KeyManager.ButtonSendOtherShareHolders
+}
+
+const buttonFinish: ButtonSubmitProperty = {
+  name: KeyManager.ButtonFinishShareholders
+}
+
+export const shareHolderManagerPresenter: ShareHolderManagerPresenter = {
+  keyHaderShareHolderManager: KeyManager.HaderShareHolderManager,
+  labelFirstName: labelFirstName,
+  labelMiddleName: labelMiddleName,
+  labelFamilyName: labelFamilyName,
+  labelPassportId: labelPassportId,
+  labelEmail: labelEmail,
+  labelTelephone: labelTelephone,
+  labelAddressCheckbox: labelAddressCheckbox,
+  labelAddressNumber: labelAddressNumber,
+  labelVillage: labelVillage,
+  labelRoad: labelRoad,
+  labelProvicne: labelProvicne,
+  labelDistrict: labelDistrict,
+  labelSubDistrict: labelSubDistrict,
+  labelZipCode: labelZipCode,
+  buttonOtherShareHolder: buttonOtherShareHolder,
+  buttonFinish: buttonFinish,
+  keyPopoverShareHolderI: KeyManager.PopoverShareHolderI,
+  keyPopoverShareHolderII: KeyManager.PopoverShareHolderII,
+  keyHeaderPopoverShareHolder: KeyManager.HeaderPopoverShareHolder
+}
