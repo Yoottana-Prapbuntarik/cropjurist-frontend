350f76f3b741a52b62dbd373fa06ffd2dfcfde40
nap3831 <nap3831@gmail.com>
2020-06-29 03:32:05 +0700

    Added new key and new redux form

--- manager/formManager.tsx
+++ manager/formManager.tsx
@@ -5,5 +5,7 @@ SignupForm = 'SignupForm',
 ForgotPasswordForm = 'ForgotPasswordForm',
 InformationForm = 'InformationForm',
 ChangePasswordForm = 'ChangePasswordForm',
-ShareInformationForm = 'ShareMyInformationForm'
+ShareInformationForm = 'ShareMyInformationForm',
+ShareHolderManagerForm = 'ShareHolderManagerForm',
+PreferenceShareManagerForm = 'PreferenceShareManagerForm'
 }
