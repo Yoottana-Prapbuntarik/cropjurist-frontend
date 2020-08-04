9cb88b0ff8ef326643d931485d99d5824d866a62
nap3831 <nap3831@gmail.com>
2020-08-04 11:44:32 +0700

    Updated key to used for call api

--- apis/nonDisclosureAPIClient.tsx
+++ apis/nonDisclosureAPIClient.tsx
@@ -7,49 +7,43 @@ export enum NonDisclosureAPIClient {
     nonDisclosureAPIClientFailed = 'nonDisclosureAPIClient_Failed'
 }
 
-export const nonDisclosureAPIClient: any = (date: string, name1: string, name2: string,
-  addressNumber1: string, road1: string,
-  province1: string, district1: string, subDistrict1: string,
-  zipcode1: string, addressNumber2: string, road2: string,
-  province2: string, district2: string, subDistrict2: string,
-  zipcode2: string, references1: string, references2: string,
-  scopeOfDiscussion: string, dateEnd: string, periodOfSecret: string,
-  arbitrationJurisdiction: string, choiceOfLaw: string,
-  companyName1: string, companyName2: string, nameOfsigning1: string,
-  nameOfsigning2: string, titleAndCapacityOfsigning1: string,
-  titleAndCapacityOfsigning2: string) => async (dispatch: Dispatch
+export const nonDisclosureAPIClient: any = (date: string,
+  disclosureName: string,
+  disclosureNameII: string,
+  address: string,
+  addressII: string,
+  referenceI: string,
+  referenceII: string,
+  endDate: string,
+  periodOfSecret: string,
+  arbitrationJurisdiction: string,
+  choiceOfLaw: string,
+  nameOfSigning1: string,
+  nameOfSigning2: string,
+  scopeOfDiscussion: string,
+  titleAndCapacityOfSigning1: string,
+  titleAndCapacityOfSigning2: string) => async (dispatch: Dispatch
 ) => {
   serviceSetToken({
     method: 'post',
     url: 'nda/',
-    data: params(date,
-      name1,
-      name2,
-      addressNumber1,
-      road1,
-      province1,
-      district1,
-      subDistrict1,
-      zipcode1,
-      addressNumber2,
-      road2,
-      province2,
-      district2,
-      subDistrict2,
-      zipcode2,
-      references1,
-      references2,
-      scopeOfDiscussion,
-      dateEnd,
+    data: params(
+      date,
+      disclosureName,
+      disclosureNameII,
+      address,
+      addressII,
+      referenceI,
+      referenceII,
+      endDate,
       periodOfSecret,
       arbitrationJurisdiction,
       choiceOfLaw,
-      companyName1,
-      companyName2,
-      nameOfsigning1,
-      nameOfsigning2,
-      titleAndCapacityOfsigning1,
-      titleAndCapacityOfsigning2)
+      nameOfSigning1,
+      nameOfSigning2,
+      scopeOfDiscussion,
+      titleAndCapacityOfSigning1,
+      titleAndCapacityOfSigning2)
   })
 
     .then((response) => {
@@ -62,105 +56,40 @@ export const nonDisclosureAPIClient: any = (date: string, name1: string, name2: 
     })
 }
 
-// export const updatenonDisclosureAPIClient: any = (currentIdInformation: number, date: string, name1: string, name2: string,
-//   addressNumber1: string, road1: string,
-//   province1: string, district1: string, subDistrict1: string,
-//   zipcode1: string, addressNumber2: string, road2: string,
-//   province2: string, district2: string, subDistrict2: string,
-//   zipcode2: string, references1: string, references2: string,
-//   scopeOfDiscussion: string, dateEnd: string, periodOfSecret: string,
-//   arbitrationJurisdiction: string, choiceOfLaw: string,
-//   companyName1: string, companyName2: string, nameOfsigning1: string,
-//   nameOfsigning2: string, titleAndCapacityOfsigning1: string,
-//   titleAndCapacityOfsigning2: string) => async (dispatch: Dispatch
-// ) => {
-//   serviceSetToken({
-//     method: 'put',
-//     url: `company_information/${currentIdInformation}/edit/`,
-//     data: params(
-//       date,
-//       name1,
-//       name2,
-//       addressNumber1,
-//       road1,
-//       province1,
-//       district1,
-//       subDistrict1,
-//       zipcode1,
-//       addressNumber2,
-//       road2,
-//       province2,
-//       district2,
-//       subDistrict2,
-//       zipcode2,
-//       references1,
-//       references2,
-//       scopeOfDiscussion,
-//       dateEnd,
-//       periodOfSecret,
-//       arbitrationJurisdiction,
-//       choiceOfLaw,
-//       companyName1,
-//       companyName2,
-//       nameOfsigning1,
-//       nameOfsigning2,
-//       titleAndCapacityOfsigning1,
-//       titleAndCapacityOfsigning2
-//     )
-//   })
-
-//     .then((response) => {
-//       if (response) {
-//         dispatch({
-//           type: NonDisclosureAPIClient.nonDisclosureAPIClientSuccess,
-//           keyMessage: KeyManager.SendInformationCompanySuccess
-//         })
-//       }
-//     })
-
-//     .catch((error) => {
-//       if (error.response) {
-//         dispatch({
-//           type: NonDisclosureAPIClient.nonDisclosureAPIClientFailed,
-//           keyMessage: error.response.data.detail
-//         })
-//       }
-//     })
-// }
-
 const params = (
   date: string,
-  name1: string,
-  name2: string,
-  addressNumber1: string, road1: string,
-  province1: string, district1: string, subDistrict1: string,
-  zipcode1: string, addressNumber2: string, road2: string,
-  province2: string, district2: string, subDistrict2: string,
-  zipcode2: string, references1: string, references2: string,
-  scopeOfDiscussion: string, dateEnd: string, periodOfSecret: string,
-  arbitrationJurisdiction: string, choiceOfLaw: string,
-  companyName1: string, companyName2: string, nameOfsigning1: string,
-  nameOfsigning2: string, titleAndCapacityOfsigning1: string,
-  titleAndCapacityOfsigning2: string
+  disclosureName: string,
+  disclosureNameII: string,
+  address: string,
+  addressII: string,
+  referenceI: string,
+  referenceII: string,
+  endDate: string,
+  periodOfSecret: string,
+  arbitrationJurisdiction: string,
+  choiceOfLaw: string,
+  nameOfSigning1: string,
+  nameOfSigning2: string,
+  scopeOfDiscussion: string,
+  titleAndCapacityOfSigning1: string,
+  titleAndCapacityOfSigning2: string
 ) => {
   return {
     date: date,
-    name_1: name1,
-    name_2: name2,
-    address_1: addressNumber1 + ' ' + road1 + ' ' + province1 + ' ' + district1 + ' ' + subDistrict1 + ' ' + zipcode1,
-    address_2: addressNumber2 + ' ' + road2 + ' ' + province2 + ' ' + district2 + ' ' + subDistrict2 + ' ' + zipcode2,
-    references_1: references1,
-    references_2: references2,
+    name_1: disclosureName,
+    name_2: disclosureNameII,
+    address_1: address,
+    address_2: addressII,
+    references_1: referenceI,
+    references_2: referenceII,
     scope_of_discussion: scopeOfDiscussion,
-    end_date: dateEnd,
+    end_date: endDate,
     period_of_secret: periodOfSecret,
     arbitration_jurisdiction: arbitrationJurisdiction,
     choice_of_law: choiceOfLaw,
-    company_name_1: companyName1,
-    company_name_2: companyName2,
-    name_of_signing_1: nameOfsigning1,
-    name_of_signing_2: nameOfsigning2,
-    title_and_capacity_of_signing_1: titleAndCapacityOfsigning1,
-    title_and_capacity_of_signing_2: titleAndCapacityOfsigning2
+    name_of_signing_1: nameOfSigning1,
+    name_of_signing_2: nameOfSigning2,
+    title_and_capacity_of_signing_1: titleAndCapacityOfSigning1,
+    title_and_capacity_of_signing_2: titleAndCapacityOfSigning2
   }
 }
